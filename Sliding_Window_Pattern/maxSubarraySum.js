dd3b02c22f927ff51bc1a990cd5a8b0649c500de
Sahil Randhawa <sahildeep.randhawa@gmail.com>
2024-03-07 12:01:50 -0500

    maxsubarrysum

---
+++ Sliding_Window_Pattern/maxSubarraySum.js
@@ -0,0 +1,61 @@
+
+// Max Subarray Sum
+
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+// Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+// Constraints:
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+// Examples:
+// maxSubarraySum([100, 200, 300, 400], 2) // 700
+// maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4) // 39
+// maxSubarraySum([-3, 4, 0, -2, 6, -1], 2) // 5
+// maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2) // 5
+// maxSubarraySum([2, 3], 3) // null
+
+// Naive Solution: Time Complexity - O(N^2)
+
+function maxSubarraySumN(arr, num) {
+    if (arr.length < num) return null;
+
+    let maxSum = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i++) {
+        let temp = 0;
+        for (let j = 0; j < num; j++) {
+            temp += arr[i + j];
+        }
+        if (temp > maxSum) {
+            maxSum = temp;
+        }
+    }
+    return maxSum;
+}
+
+// Refactored Solution: Time Complexity - O(N)
+
+function maxSubarraySum(arr, num) {
+    if (arr.length < num) return null;
+
+    let maxSum = 0;
+    let tempSum = 0;
+
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+
+    tempSum = maxSum;
+
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSubarraySum([100, 200, 300, 400], 2)) // 700
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)) // 39
+console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)) // 5
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)) // 5
+console.log(maxSubarraySum([2, 3], 3)) // null
