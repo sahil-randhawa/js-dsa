9f505d4db1004b47636d6585d2503d9723c97d5f
Sahil Randhawa <98905306+sahil-randhawa@users.noreply.github.com>
2024-03-09 23:57:44 -0500

    Update minSubarrayLength.js

--- Sliding_Window_Pattern/minSubarrayLength.js
+++ Sliding_Window_Pattern/minSubarrayLength.js
@@ -13,7 +13,6 @@
 // minSubarrayLen([2, 1, 6, 5, 4], 9) // 2 -> because [5, 4] is the smallest subarray
 // minSubarrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52) // 1 -> because [62] is greater than 52
 // minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39) // 3
-// minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55) // 5
 // minSubarrayLen([4, 3, 3, 8, 1, 2, 3], 11) // 2
 // minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) // 0
 
